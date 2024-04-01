996b97008ab2137d01402b276fb459af1bcf2724
MadnessFalax <petr.aujezdsky@gmail.com>
2024-04-01 12:36:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
