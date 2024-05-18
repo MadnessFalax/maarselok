7ffee629736a71852bdae1a635663ef49a8e3c62
MadnessFalax <petr.aujezdsky@gmail.com>
2024-05-18 17:46:50 +0200

    Added new attribute

---
+++ data/view_attributes/ViewNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_projekt.data.view_attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Property)]
+    public class ViewNameAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public ViewNameAttribute(string view_name)
+        {
+            Name = view_name;
+        }
+    }
+}
